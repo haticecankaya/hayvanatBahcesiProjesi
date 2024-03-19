b66c6c9fac08a887428f2361aba38df3f53b0986
Hatice Çankaya <81267955+haticecankaya@users.noreply.github.com>
2024-03-19 11:51:03 +0300

    first commit

---
+++ animalProject.py
@@ -0,0 +1,8 @@
+#First hunting process then breeding process 
+# The animals can be locate same location.
+# Hunting order: Hunter, Lion, Wolf
+# Breeding order: Sheep,Cow,Chicken, Lion
+# Process continue until  the number 1000 is exceeded.
+# For controling the process, program write every step console.
+# OOP will be used for the maintenance process
+
