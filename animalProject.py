35c03db70e7304bac8e83ab554034ca79ab419be
Hatice Çankaya <81267955+haticecankaya@users.noreply.github.com>
2024-03-20 04:49:58 +0300

    add findDistance(), killAnimal(), huntingTime() functions

--- animalProject.py
+++ animalProject.py
@@ -1,7 +1,7 @@
 #First hunting process then breeding process 
 # The animals can be locate same location.
-# Hunting order: Hunter, Lion, Wolf
-# Breeding order: Sheep,Cow,Chicken,Wolf Lion
+# Hunting order: The first to be created is the first to be hunted.
+# Breeding order: The first to be created is the first to breed. 
 # Process continue until  the number 1000 is exceeded.
 # For controling the process, program write every step console.
 # OOP will be used for the maintenance process
@@ -80,6 +80,7 @@ def show_data(Animal):
   print("LocateY:", Animal.locateY)
   print("MotionUnitNumber:", Animal.motionUnitNumber)
   print("Gender:", Animal.gender)
+  print("IsAlive:", Animal.isAlive)
 
 allAnimals=[]
 allHunters=[]
@@ -137,10 +138,75 @@ def createFirstAnimals(hunter, sheepF, sheepM, cowF, cowM, chickenF, chickenM, w
     allAnimals.append(yeni_nesne)
     allHunters.append(yeni_nesne)
     
-createFirstAnimals(1, 2, 3, 1, 2, 4, 2, 5, 6, 2, 1)
+createFirstAnimals(1, 15, 15, 10, 10, 10, 10, 5, 5, 4, 4)
 
+#createFirstAnimals() Test Block
+# for i in allAnimals:
+#   if i.animalType=="Lion":
+#      show_data(i)
 
-for i in allAnimals:
-  if i.animalType=="Lion":
-     show_data(i)
+def findDistance(firstAnimal,secondAnimal):
+   x=abs(firstAnimal.locateX -secondAnimal.locateX )
+   y=abs(firstAnimal.locateY -secondAnimal.locateY )
+   return x+y
+
+#findDistance() Test Block
+# obj1=Animal(animalType="Wolf", locateX=0,locateY=7,gender="F")
+# obj2=Animal(animalType="Wolf", locateX=2,locateY=0,gender="F")
+# print(findDistance(obj1,obj2))
+
+def killAnimal(animal,animalList):
+   animal.isAlive= False
+   animalList.remove(animal)
+
+# killAnimal() Test Block
+# change **this areas** and try this code for control
+# obj1=Animal(**animalType="Wolf", locateX=0,locateY=7,gender="F"**)
+# obj2=Animal(**animalType="Wolf", locateX=2,locateY=0,gender="F"**)
+# obj3=Animal(**animalType="Sheep", locateX=2,locateY=0,gender="F"**)
+# exampleList=[obj1,obj2,obj3]
+# for i in exampleList:
+#    if i.**locateX**==0:
+#       killAnimal(i,exampleList)
+# print(exampleList)
+# print(exampleList[0].**locateX**)
+# print(exampleList[1].**locateX**)
+# print(**obj1**.isAlive)
+
+def huntingTime(animalList, hunterList):  
+   print("****START HUNTING TIME****")
+   print("The wolf can hunt at a distance of 4 and less than 4 units, the lion at a distance of 5 and less than 5 units, and the hunter at a distance of only 8 units.")
+   for i in hunterList:
+      if i.animalType=="Wolf":
+         for j in animalList:
+            if j.animalType=="Sheep" or j.animalType=="Chicken":
+               if findDistance(i,j)<=i.huntingUnitNumber:
+                  killAnimal(j,allAnimals)
+                  show_data(i)
+                  show_data(j)
+                  print("**********")
+      if i.animalType=="Lion":
+         for j in animalList:
+            if j.animalType=="Sheep" or j.animalType=="Cow":
+               if findDistance(i,j)<=i.huntingUnitNumber:
+                  killAnimal(j,allAnimals)
+                  show_data(i)
+                  show_data(j)
+                  print("**********")
+      if i.animalType=="Hunter":
+         for j in animalList:
+            if findDistance(i,j)==i.huntingUnitNumber:
+               #The hunter is an archer woman, so she can only kill the animal from a distance of 8 units. 
+               #Because that's how I imagined it.
+               killAnimal(j,allAnimals)
+               if j.animalType=="Lion" or j.animalType=="Wolf":
+                  killAnimal(j,allHunters)
+               show_data(i)
+               show_data(j)
+               print("**********")         
+   print("****FINISH HUNTING TIME****")
+
+#huntingTime() works but the code is difficult to maintain. There is a danger of being affected by any change.
+
+huntingTime(allAnimals,allHunters)
 
