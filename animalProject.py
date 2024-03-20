61b197b0f7f0eaf7b7891814248de59bd4e138a3
Hatice Çankaya <81267955+haticecankaya@users.noreply.github.com>
2024-03-20 06:32:22 +0300

    Finish project

--- animalProject.py
+++ animalProject.py
@@ -5,7 +5,15 @@
 # Process continue until  the number 1000 is exceeded.
 # For controling the process, program write every step console.
 # OOP will be used for the maintenance process
-# huntingUnitNumber=None, breedingUnitNumber=None, MotionUnitNumber=None, isAlive=None,
+# I choose the camelCase style.
+
+#At first, I tried to make a simple interface so that it would be easy to control. 
+#This made it easier for me to check while writing code. 
+#However, the project eventually caused problems. That's why I put those parts in the comment line.
+
+#Sometimes it can take a long time for the project to yield results. Not because of this mistake. 
+#This is happening because the number of animals increases. Please wait. 
+
 import random
 
 def chooseRandomLocation():
@@ -174,43 +182,48 @@ def killAnimal(animal,animalList):
 # print(**obj1**.isAlive)
 
 def huntingTime(animalList, hunterList):  
-   print("****START HUNTING TIME****")
-   print("The wolf can hunt at a distance of 4 and less than 4 units, the lion at a distance of 5 and less than 5 units, and the hunter at a distance of only 8 units.")
+   deadNumber=0
+  #  print("****START HUNTING TIME****")
+  #  print("The wolf can hunt at a distance of 4 and less than 4 units, the lion at a distance of 5 and less than 5 units, and the hunter at a distance of only 8 units.")
    for i in hunterList:
       if i.animalType=="Wolf":
          for j in animalList:
             if j.animalType=="Sheep" or j.animalType=="Chicken":
                if findDistance(i,j)<=i.huntingUnitNumber:
                   killAnimal(j,allAnimals)
-                  show_data(i)
-                  show_data(j)
-                  print("**********")
+                  deadNumber=deadNumber+1
+                  # show_data(i)
+                  # show_data(j)
+                  # print("**********")
       if i.animalType=="Lion":
          for j in animalList:
             if j.animalType=="Sheep" or j.animalType=="Cow":
                if findDistance(i,j)<=i.huntingUnitNumber:
                   killAnimal(j,allAnimals)
-                  show_data(i)
-                  show_data(j)
-                  print("**********")
+                  deadNumber=deadNumber+1
+                  # show_data(i)
+                  # show_data(j)
+                  # print("**********")
       if i.animalType=="Hunter":
          for j in animalList:
             if findDistance(i,j)==i.huntingUnitNumber:
                #The hunter is an archer woman, so she can only kill the animal from a distance of 8 units. 
                #Because that's how I imagined it.
                killAnimal(j,allAnimals)
+               deadNumber=deadNumber+1
                if j.animalType=="Lion" or j.animalType=="Wolf":
                   killAnimal(j,allHunters)
-               show_data(i)
-               show_data(j)
-               print("**********")         
-   print("****FINISH HUNTING TIME****")
+  #              show_data(i)
+  #              show_data(j)
+  #              print("**********")         
+  #  print("****FINISH HUNTING TIME****")
+   return deadNumber
 
-#huntingTime() works but the code is difficult to maintain. There is a danger of being affected by any change.
+#huntingTime() works but the code is difficult to maintain and control. There is a danger of being affected by any change.
 
 def checkBreedingConditions(one,two):
   if one.animalType == two.animalType and one.gender != two.gender:
-     if findDistance(one,two) <=3:
+     if findDistance(one,two) <=one.breedingUnitNumber:
         return True
      else:
         return False
@@ -223,3 +236,73 @@ def checkBreedingConditions(one,two):
 # obj2=Animal(**animalType="Wolf", locateX=3,locateY=7,gender="F"**)
 # print(checkBreedingConditions(obj1,obj2))
   
+
+def breedingTime(mainAnimalList):
+   babyNumber=0
+  #  print("****START BREEDİNG TIME****")
+   breedingList=[]
+   babyList=[]
+   for i in mainAnimalList:
+      breedingList.append(i)
+   for j in breedingList:
+      for k in breedingList:
+         if checkBreedingConditions(j,k)==True:
+            # show_data(j)
+            # show_data(k)
+            breedingList.remove(k)
+            baby=Animal(animalType=j.animalType, locateX=chooseRandomLocation(),locateY=chooseRandomLocation(),gender=chooseRandomGender())
+            babyList.append(baby)
+  #  print("***Baby List***")
+   for baby in babyList:
+      # show_data(baby)
+      allAnimals.append(baby)
+      babyNumber=babyNumber+1
+      if baby.animalType=="Wolf" or baby.animalType=="Lion":
+         allHunters.append(baby)
+  #  print("****FINISH BREEDİNG TIME****")
+   return babyNumber
+
+#breedingTime() works but the code is difficult to control.
+
+def chooseAndMotion(animalList):
+   x=random.randint(0,len(animalList)-1) #choose random animal for motion
+   y=animalList[x]
+   z=random.randint(0,1) #choose x or y axis
+   p=random.randint(0,1) #choose positive or negative side
+  #  print("BEFORE MOTION")
+  #  show_data(y)
+   if z==0 and p==0:
+      y.locateX= y.locateX-y.motionUnitNumber
+   elif z==0 and p==1:
+      y.locateX= y.locateX+y.motionUnitNumber
+   elif z==1 and p==0:
+      y.locateY= y.locateY-y.motionUnitNumber
+   elif z==1 and p==1:
+      y.locateY= y.locateY+y.motionUnitNumber
+  #  print("AFTER MOTION")
+  #  show_data(y)
+   return y.motionUnitNumber
+
+totalUnits=0
+deadAnimalNumber=0
+newBabyNumber=0
+
+while totalUnits<1000:
+   deadAnimalNumber=deadAnimalNumber+huntingTime(allAnimals, allHunters)
+   newBabyNumber=newBabyNumber+breedingTime(allAnimals)
+   totalUnits=totalUnits+chooseAndMotion(allAnimals)
+
+huntingTime(allAnimals, allHunters)
+
+print("*********************************")
+print("Süreç Sonunda Toplam Hayvan Sayısı:")
+print(len(allAnimals))
+print("Süreç Sonunda Ölen Toplam Hayvan Sayısı:")
+print(deadAnimalNumber)
+print("Süreç Sonunda Doğan Toplam Hayvan Sayısı:")
+print(newBabyNumber)
+print("Başlangıçtaki Hayvan Sayısı")
+startNumber=len(allAnimals)+deadAnimalNumber-newBabyNumber
+print(startNumber)
+print("Toplam Hareket:")
+print(totalUnits)
